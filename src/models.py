4170124e6de278a584a74df73eb3b69cc10b87b4
AliBagheriNejad <abneng1397@gmail.com>
2025-10-19 10:28:05 +0330

    do something about concatting and channels and stuff

--- src/models.py
+++ src/models.py
@@ -288,20 +288,18 @@ class Up(nn.Module):
         self.up = nn.ConvTranspose1d(in_channel, in_channel//2, kernel_size=us, stride=us)
         self.conv1 = ConvBlock(in_channel, out_channel)
         self.conv2 = ConvBlock(out_channel, out_channel)
-        # self.conv3 = ConvBlock(out_channel, out_channel)
-        # self.conv4 = ConvBlock(out_channel, out_channel)
-        # self.conv5 = ConvBlock(out_channel, out_channel)
-        # self.conv6 = ConvBlock(out_channel, out_channel)
 
     def forward(self, x_u, x):
-        x_u = self.up(x_u)
-        x = torch.concat([x_u,x], dim=1)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x)) 
-        # x = F.relu(self.conv3(x))
-        # x = F.relu(self.conv4(x))
-        # x = F.relu(self.conv5(x))
-        # x = F.relu(self.conv6(x))
+        if x is not None:
+            x_u = self.up(x_u)
+            x = torch.concat([x_u,x], dim=1)
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x)) 
+        else:
+            x_u = self.up(x_u)
+            x = torch.concat([x_u,x_u], dim=1)
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x)) 
 
         return x
 
@@ -310,17 +308,29 @@ class ConvEmbed(nn.Module):
     def __init__(self, in_channel=1, out_channel=1, last_layer=False, mp=None,drop=None, ks=3, pad=1):
         super().__init__()
 
-        self.conv2 = ConvBlock(out_channel,out_channel, ks=ks, pad=pad, mp=mp, drop=drop)
         self.conv1 = ConvBlock(in_channel, out_channel, ks=ks, pad=pad, mp=mp, drop=drop)
+        self.conv2 = ConvBlock(out_channel,out_channel, ks=ks, pad=pad, mp=mp, drop=drop)
 
         self.last_layer = last_layer
 
+        self.ch_keep = True if in_channel==out_channel else False
+
     def forward(self,x):
-        x = F.relu(self.conv1(x)) + x
-        if not self.last_layer:
-            x = F.relu(self.conv2(x)) + x
+
+        if self.ch_keep:
+
+            x = F.relu(self.conv1(x)) + x
+            if not self.last_layer:
+                x = F.relu(self.conv2(x)) + x
+            else:
+                x = self.conv2(x)
         else:
-            x = self.conv2(x)
+            
+            x = F.relu(self.conv1(x))
+            if not self.last_layer:
+                x = F.relu(self.conv2(x)) 
+            else:
+                x = self.conv2(x)
 
         return x
 
@@ -334,18 +344,18 @@ class UNET(BaseModel):
     ):
         super().__init__()
 
-        self.d1_z = Down(in_channel_z, 64, mp=1) # 1024
+        self.d1_z = Down(in_channel_z, 64, mp=4) # 1024
         self.d2_z = Down(64, 128, mp=4) # 256
-        self.d3_z = Down(128, 256, mp=4) # 64
+        self.d3_z = Down(128, 256) # 64
         self.d4_z = Down(256, 512) # 32
-        self.d5_z = Down(512, 1024) # 16
 
-        self.d1_x = Down(in_channel_x, 64, mp=1)
+        self.d1_x = Down(in_channel_x, 64, mp=4)
         self.d2_x = Down(64, 128, mp = 4)
-        self.d3_x = Down(128, 256, mp=4)
+        self.d3_x = Down(128, 256)
         self.d4_x = Down(256, 512)
-        self.d5_x = Down(512, 1024)
 
+        self.kz_0 = ConvEmbed(512, 1024)
+        self.kx_0 = ConvEmbed(1024, 2048)
         self.kz = ConvEmbed(1024, 1024)
         self.kx = ConvEmbed(2048, 2048)
 
@@ -386,10 +396,6 @@ class UNET(BaseModel):
         z4 = z4 + sinusoidal_embedding(t, z4.shape[2]).unsqueeze(1)
         z4_d = z4_d + sinusoidal_embedding(t, z4_d.shape[2]).unsqueeze(1)
 
-        z5, z5_d = self.d5_z(z4_d)
-        # z5 = z5 + sinusoidal_embedding(t, z5.shape[2]).unsqueeze(1)
-        # z5_d = z5_d + sinusoidal_embedding(t, z5_d.shape[2]).unsqueeze(1)
-
         # Downsampilng X
         x1, x1_d = self.d1_x(x)
         # x1 = x1 + sinusoidal_embedding(t, x1.shape[2]).unsqueeze(1)
@@ -405,23 +411,22 @@ class UNET(BaseModel):
         
         x4, x4_d = self.d4_x(x3_d)
         x4 = x4 + sinusoidal_embedding(t, x4.shape[2]).unsqueeze(1)
-        x4_d = x4_d + sinusoidal_embedding(t, x4_d.shape[2]). unsqueeze(1)
-
-        x5, x5_d = self.d5_x(x4_d)
-        # x5 = x5 + sinusoidal_embedding(t, x5.shape[2]).unsqueeze(1)
-        # x5_d = x5_d + sinusoidal_embedding(t, x5_d.shape[2]). unsqueeze(1)              
+        x4_d = x4_d + sinusoidal_embedding(t, x4_d.shape[2]). unsqueeze(1) # x5_d = x5_d + sinusoidal_embedding(t, x5_d.shape[2]). unsqueeze(1)              
         # Concat Low
-        z7 = z5_d
-        x7 = torch.concat([x5_d, z7], dim=1)
+        z5 = z4_d
+        x5 = torch.concat([x4_d, z5], dim=1)
 
         # Process Low
-        z7 = self.kz(z7)
+        z5 = self.kz_0(z5)
+        x5 = self.kx_0(x5)
+
+        z7 = self.kz(z5)
         z7 = z7 + sinusoidal_embedding(t, z7.shape[2]).unsqueeze(1)
-        x7 = self.kx(x7)
+        x7 = self.kx(x5)
         x7 = x7 + sinusoidal_embedding(t, x7.shape[2]).unsqueeze(1)
 
         # Upsampling Z
-        z4_u = self.u4_z(z7, z5)
+        z4_u = self.u4_z(z7, None)
         # z4_u = z4_u + sinusoidal_embedding(t, z4_u.shape[2]).unsqueeze(1)
 
         z3_u = self.u3_z(z4_u, z4)
